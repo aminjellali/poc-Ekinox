ba01306caf7f2d176ee0bcdb25c2a5177a5ab946
Amine JALLALI <aminejallali@pc-436.home>
2024-03-06 20:23:16 +0100

    init project

---
+++ src/main/java/com/github/saga_pricer/Main.java
@@ -0,0 +1,7 @@
+package com.github.saga_pricer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/github/saga_pricer/Pricer.java
@@ -0,0 +1,4 @@
+package com.github.saga_pricer;
+
+public class Pricer {
+}
